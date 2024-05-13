15a3cd31261073d68293bb212e2dfca649ec88bb
Claude Jordan <claudejordan@gmail.com>
2024-05-13 09:09:41 +0200

    initial commit, basic server setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.json())
+
+const gasStationDataUrl = "https://geoportal.stadt-koeln.de/arcgis/rest/services/verkehr/gefahrgutstrecken/MapServer/0/query?where=objectid+is+not+null&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&havingClause=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&historicMoment=&returnDistinctValues=false&resultOffset=&resultRecordCount=&returnExtentOnly=false&datumTransformation=&parameterValues=&rangeValues=&quantizationParameters=&featureEncoding=esriDefault&f=pjson";
+
+const saveGasStationDataToDatabase = (url) => {
+	// save everything to postgres database
+}
+
+app.listen(3000, () => {
+	saveGasStationDataToDatabase(gasStationDataUrl)
+})
