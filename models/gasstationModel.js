172db8ae60d572ea15cb7e5e390e096b340b0ce9
Claude Jordan <claudejordan@gmail.com>
2024-05-14 08:52:05 +0200

    updated code of gasstationModel.js to be more in line with gasstationController.js

--- models/gasstationModel.js
+++ models/gasstationModel.js
@@ -1,40 +1,33 @@
 const db = require("../config/config.js");
 
-// Use this empty object for exporting the functions below
-const GasStation = {};
-
-GasStation.create = (id, address, x, y) => {
-  return db.none(`insert into gasstations values ($1, $2, $3, $4);`, [
-    id,
-    address,
-    x,
-    y,
-  ]);
+module.exports = {
+  create(id, address, x, y) {
+    return db.none(`insert into gasstations values ($1, $2, $3, $4);`, [
+      id,
+      address,
+      x,
+      y,
+    ]);
+  },
+  getAll() {
+    return db.any("select * from gasstations;");
+  },
+  getById(id) {
+    return db.any(`select * from gasstations where id = $1;`, [id]);
+  },
+  update(id, address, x, y) {
+    return db.none(
+      `update gasstations
+              set
+                  address = coalesce($2, address),
+                  x = coalesce($3, x),
+                  y = coalesce($4, y)
+              where
+                  id = $1;`,
+      [id, address, x, y],
+    );
+  },
+  delete(id) {
+    return db.none(`delete from gasstations where id = $1;`, [id]);
+  },
 };
-
-GasStation.getAll = () => {
-  return db.any("select * from gasstations;");
-};
-
-GasStation.getById = (id) => {
-  return db.any(`select * from gasstations where id = $1;`, [id]);
-};
-
-GasStation.update = (id, address, x, y) => {
-  return db.none(
-    `update gasstations
-        		set
-                address = coalesce($2, address),
-                x = coalesce($3, x),
-                y = coalesce($4, y)
-            where
-                id = $1;`,
-    [id, address, x, y],
-  );
-};
-
-GasStation.delete = (id) => {
-  return db.none(`delete from gasstations where id = $1;`, [id]);
-};
-
-module.exports = GasStation;
